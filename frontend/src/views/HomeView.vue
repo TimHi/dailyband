88986f8531cd1ca20c69a82864d615b6497e76f4
TimHi <TimHiller@live.de>
2024-01-28 00:51:58 +0100

    styling a bit

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -7,12 +7,36 @@ const dailyAlbum = getDailyAlbum
 </script>
 
 <template>
-  <main>
-    <h1>You might like</h1>
-    <a :href="'https://daily.bandcamp.com/' + dailyAlbum.link">
-      <img :src="dailyAlbum.image" />
-      <h2>{{ dailyAlbum.artist }} - {{ dailyAlbum.title }}</h2>
-      <h3>Daily Album from {{ dailyAlbum.date }}</h3>
-    </a>
+  <main class="center">
+    <div class="border">
+      <img class="item" :src="dailyAlbum.image" />
+      <div>
+        <h1 class="item">{{ dailyAlbum.artist }} - {{ dailyAlbum.title }}</h1>
+        <h2 class="item">
+          Daily Album from {{ dailyAlbum.date }} -
+          <a :href="'https://daily.bandcamp.com/' + dailyAlbum.link">Link 🔗</a>
+        </h2>
+      </div>
+    </div>
   </main>
 </template>
+
+<style>
+.center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+.border {
+  border: 1px solid #383a3f;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  padding: 10px; /* Add some padding for better visual appearance */
+  text-align: center; /* Center text horizontally */
+  height: auto;
+  width: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
