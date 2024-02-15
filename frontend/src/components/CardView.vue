b0f0280261aeede9d426bd5418fd526fdc37808e
TimHi <TimHiller@live.de>
2024-02-15 23:31:51 +0100

    css

--- frontend/src/components/CardView.vue
+++ frontend/src/components/CardView.vue
@@ -4,68 +4,77 @@ import { useAlbumStore } from '@/stores/album'
 const albumStore = useAlbumStore()
 const { getDailyAlbum } = albumStore
 const dailyAlbum = getDailyAlbum
-
 </script>
 
 <template>
-    <main class="center">
-        <div class="card">
-            <img class="item" :src="dailyAlbum.image" />
-            <div class="container">
-                <h4><b>John Doe</b></h4>
-                <p>Architect & Engineer</p>
-            </div>
-        </div>
-    </main>
+  <main class="center">
+    <div class="card">
+      <h1>Try this tune</h1>
+      <p>Daily Camp's Musical Recommendation</p>
+      <h2>{{ dailyAlbum.date }}</h2>
+      <img class="item" :src="dailyAlbum.image" />
+      <div class="container">
+        <h3>
+          <b>{{ dailyAlbum.title }}</b>
+        </h3>
+        <h4>by {{ dailyAlbum.artist[0] }}</h4>
+        <p>
+          Lorem ipsum der aal Aalt ich brauch hier ein wenig Content am besten so viel das es
+          Overflowt also schreib ich hier einfach mal weiter. Sollte passen jetzt. Oder ein zwei
+          Sätze mehr sollten auch nicht wehtun.
+        </p>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style>
 img {
-    border-radius: 5%;
+  border-radius: 5%;
 }
 
 .card {
-    /* Add shadows to create the "card" effect */
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-    transition: 0.3s;
-    background-color: aqua;
-    border-radius: 5%;
+  /* Add shadows to create the "card" effect */
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  background-color: aqua;
+  border-radius: 2%;
 }
 
 /* On mouse-over, add a deeper shadow */
 .card:hover {
-    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+  box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
 }
 
 /* Add some padding inside the card container */
 .container {
-    padding: 2px 16px;
+  padding: 2px 16px;
 }
 
 .center {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
 }
 
 .container {
-    max-width: 400px;
+  max-width: 400px;
 
-    flex-wrap: wrap;
+  flex-wrap: wrap;
 }
 
 .border {
-    border: 1px solid #383a3f;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    padding: 10px;
-    /* Add some padding for better visual appearance */
-    text-align: center;
-    /* Center text horizontally */
-    height: auto;
-    width: auto;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  border: 1px solid #383a3f;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  padding: 10px;
+  /* Add some padding for better visual appearance */
+  text-align: center;
+  /* Center text horizontally */
+  height: auto;
+  width: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
